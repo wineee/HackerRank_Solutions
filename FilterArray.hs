164ff74fefe40d9630aab243cf89a3f378c10772
wineee <1758075541@qq.com>
2021-05-23 17:08:10 +0800

    Add FilterArray

---
+++ FilterArray.hs
@@ -0,0 +1,11 @@
+f :: Int -> [Int] -> [Int]
+f n arr = [val | val <- arr, val < n]
+--Fill up this function
+
+-- The Input/Output section. You do not need to change or modify this part
+main = do 
+    n <- readLn :: IO Int 
+    inputdata <- getContents 
+    let 
+        numbers = map read (lines inputdata) :: [Int] 
+    putStrLn . unlines $ (map show . f n) numbers
