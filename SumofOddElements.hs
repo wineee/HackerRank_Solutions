714b3f4106e5a22e01858ff05b19bf165edf5628
wineee <1758075541@qq.com>
2021-05-23 18:16:36 +0800

    Add Sum of Odd Elements

---
+++ SumofOddElements.hs
@@ -0,0 +1,7 @@
+f arr = sum [x | x <- arr, odd x]
+-- Fill up this function body
+
+-- This part handles the Input/Output and can be used as it is. Do not change or modify it.
+main = do
+	inputdata <- getContents
+	putStrLn $ show $ f $ map (read :: String -> Int) $ lines inputdata
